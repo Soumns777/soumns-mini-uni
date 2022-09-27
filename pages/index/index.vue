93317e82418767d41067f241135e5b504d3e5329
soumns <2794122209@qq.com>
2022-09-27 17:49:36 +0800

    🎉 feat:init-mini-program

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="container">
+    <button class="container-btn soumns-flex" type="primary" loading="true">
+      跳转
+    </button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  .container-btn {
+    width: 450rpx;
+    height: 68rpx;
+    background: #3184fe;
+    border-radius: 34rpx;
+    background-color: #3184fe;
+  }
+}
+</style>
